46d9e13a0f543798db2461c86482d51ed6e85eae
yyoshidaweb <yoshidayumajob@gmail.com>
2025-11-22 12:49:19 +0900

    feat: Userモデルを作成し、OAuth用メソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseで利用する機能を指定（DB認証、登録、パスワードリセット、セッション保持、メール検証、外部認証）
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [ :google_oauth2 ]
+
+  # Googleログイン時に呼ばれるユーザー検索 or 生成メソッド
+  def self.from_omniauth(auth)
+    # providerとuidの一致するユーザーを検索し、存在しない場合は新規作成
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email # Googleから取得したメールアドレスを設定
+      user.password = Devise.friendly_token[0, 20] # ランダムなパスワードを自動生成
+      user.name = auth.info.name if auth.info.respond_to?(:name) # 名前があれば設定
+    end
+  end
 end
