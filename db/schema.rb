a86886e7454fcac42ebfc572c7194878f32d9499
yyoshidaweb <yoshidayumajob@gmail.com>
2025-11-22 21:55:48 +0900

    feat: Userにnameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_22_024121) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_22_101653) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "provider"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
