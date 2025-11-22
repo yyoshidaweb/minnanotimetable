1fed7e03019c2e1ea6084cb174ac6a970e37e9ae
yyoshidaweb <yoshidayumajob@gmail.com>
2025-11-22 11:55:02 +0900

    feat: UserモデルにOAuth用カラムを追加し、マイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_22_003313) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_22_024121) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "provider"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "uid"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
