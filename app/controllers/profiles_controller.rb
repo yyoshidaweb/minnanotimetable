3dcfaf50cf02c40fb4c97dd5a1973cdad81426d4
yyoshidaweb <yoshidayumajob@gmail.com>
2025-11-26 04:56:21 +0900

    refactor: プロフィールコントローラーからflashメッセージについての記述を削除

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,9 +19,8 @@ class ProfilesController < ApplicationController
     if @user.update(user_params)
       redirect_to profile_path, notice: "プロフィールを更新しました。"
     else
-      # モデルのバリデーションエラーを alert に渡す
-      flash.now[:alert] = @user.errors.full_messages.join(", ")
       # 更新に失敗した場合、編集ページを再表示
+      @page_title = "プロフィール編集"
       render :edit, status: :unprocessable_entity
     end
   end
