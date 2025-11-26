27b23f99876cb7f911c49b663c5002e8a9248755
yyoshidaweb <yoshidayumajob@gmail.com>
2025-11-26 20:25:26 +0900

    feat: userモデルにdescriptionカラムを追加してマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_26_035843) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_26_111859) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.text "description"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name"
