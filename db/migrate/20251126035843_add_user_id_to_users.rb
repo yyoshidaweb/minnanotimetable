e23d289480f9b317c4376f5225515d80f83b456f
yyoshidaweb <yoshidayumajob@gmail.com>
2025-11-27 18:17:13 +0900

    feat: userにフィールドを追加するマイグレーションファイルを一つに集約

--- db/migrate/20251126035843_add_user_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToUsers < ActiveRecord::Migration[8.1]
-  def change
-    add_column :users, :user_id, :string, null: false
-    add_index :users, :user_id, unique: true
-  end
-end
---
+++ db/migrate/20251127085954_add_user_fields_to_users.rb
@@ -0,0 +1,16 @@
+class AddUserFieldsToUsers < ActiveRecord::Migration[8.1]
+  def change
+    # 名前と説明欄、ユーザーID
+    add_column :users, :name, :string, null: false, default: "", limit: 50
+    add_column :users, :description, :text
+    add_column :users, :user_id, :string, null: false, default: "", limit: 50
+
+    # Omniauth関連
+    add_column :users, :provider, :string, null: false, default: "", limit: 50
+    add_column :users, :uid, :string, null: false, default: "", limit: 255
+
+    # ユニーク制約
+    add_index  :users, [ :provider, :uid ], unique: true
+    add_index  :users, :user_id, unique: true
+  end
+end
