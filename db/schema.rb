9b24d7f113f26edbf4548b2949b9a16c7da317c3
yyoshidaweb <yoshidayumajob@gmail.com>
2025-12-01 02:56:53 +0900

    feat: performer_favoritesをマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_30_174639) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_30_175200) do
   create_table "days", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.date "date", null: false
@@ -61,6 +61,16 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_174639) do
     t.index ["day_id"], name: "index_performances_on_day_id"
     t.index ["performer_id"], name: "index_performances_on_performer_id"
     t.index ["stage_id"], name: "index_performances_on_stage_id"
+  end
+
+  create_table "performer_favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "performer_id", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["performer_id"], name: "index_performer_favorites_on_performer_id"
+    t.index ["user_id", "performer_id"], name: "index_performer_favorites_on_user_id_and_performer_id", unique: true
+    t.index ["user_id"], name: "index_performer_favorites_on_user_id"
   end
 
   create_table "performer_name_tags", force: :cascade do |t|
@@ -126,6 +136,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_174639) do
   add_foreign_key "performances", "days"
   add_foreign_key "performances", "performers"
   add_foreign_key "performances", "stages"
+  add_foreign_key "performer_favorites", "performers"
+  add_foreign_key "performer_favorites", "users"
   add_foreign_key "performers", "events"
   add_foreign_key "performers", "performer_name_tags"
   add_foreign_key "stages", "events"
