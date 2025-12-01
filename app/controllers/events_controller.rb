00a80f10e14670cb13bb63e83f5cfd6160a5916d
yyoshidaweb <yoshidayumajob@gmail.com>
2025-12-01 20:22:16 +0900

    feat: タイムテーブル詳細ページの最低限のビューを作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,11 +2,19 @@ class EventsController < ApplicationController
   def show
     # event_key でイベントを取得
     @event = Event.find_by!(event_key: params[:event_key])
-
-    # 関連データをまとめて取得（必要に応じて）
+    # 関連データをまとめて取得
     @days = @event.days.order(:date)
     @performers = @event.performers
     @stages = @event.stages
-    @page_title = @event.event_name_tag.name
+
+    # クエリに ?d がある場合はそれを優先、なければ最古日付を採用
+    @selected_date = params[:d].present? ? Date.parse(params[:d]) : @days.first.date
+
+    # 対象日の performances のみ取得
+    @performances = @event.performances
+                          .joins(:day)
+                          .where(days: { date: @selected_date })
+                          .includes(:performer, :stage)
+                          .order(:start_time)
   end
 end
