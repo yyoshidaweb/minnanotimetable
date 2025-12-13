248e555d33b97e7d8b4b6c2e905d9316f9190ee0
yyoshidaweb <yoshidayumajob@gmail.com>
2025-12-13 11:51:34 +0900

    fix: BrakemanによるCross-Site Scriptingの警告を解消

--- app/helpers/performers_helper.rb
+++ app/helpers/performers_helper.rb
@@ -1,2 +1,9 @@
 module PerformersHelper
+  # URLを検証・整形する
+  def safe_website_link(performer, **options)
+    return unless performer.website_url.present?
+    url = URI.parse(performer.website_url) rescue nil
+    return unless url
+    link_to url.to_s, url.to_s, **options
+  end
 end
