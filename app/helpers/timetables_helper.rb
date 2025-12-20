27bbb6ddaa5951d7c222d54dc1583ecf7bf042ad
yyoshidaweb <yoshidayumajob@gmail.com>
2025-12-20 20:47:06 +0900

    feat: 仮の実装として1行30分に固定

--- app/helpers/timetables_helper.rb
+++ app/helpers/timetables_helper.rb
@@ -6,10 +6,27 @@ module TimetablesHelper
     (start_h..end_h).to_a
   end
 
-  # 指定時間・ステージの出演情報を取得
-  def performance_at(performances_by_stage, stage_id, hour)
-    performances_by_stage[stage_id]&.find do |p|
-      p.start_time.hour == hour
+  # performance がこの時間枠に入るか
+  def performance_at(stage_performances, stage_id, hour, minute)
+    return nil unless stage_performances[stage_id]
+
+    slot_start = hour * 60 + minute
+    slot_end   = slot_start + 30
+
+    stage_performances[stage_id].find do |p|
+      p.start_key >= slot_start && p.start_key < slot_end
+    end
+  end
+
+  # 30分刻みの時刻スロットを生成
+  def time_slots_for_timetable(performances)
+    start_min =
+      performances.min_by(&:start_time).start_time.hour * 60
+    end_min =
+      performances.max_by(&:end_time).end_time.hour * 60 + 30
+
+    (start_min..end_min).step(30).map do |minute|
+      [ minute / 60, minute % 60 ]
     end
   end
 end
