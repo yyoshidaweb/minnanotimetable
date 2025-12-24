a9fd77d442a7f0480a39d3a3754f0a4920a3a39e
yyoshidaweb <yoshidayumajob@gmail.com>
2025-12-24 15:27:40 +0900

    feat: アイコン塗りつぶし判定をヘルパーに移動

--- app/helpers/bottom_nav_helper.rb
+++ app/helpers/bottom_nav_helper.rb
@@ -1,18 +1,7 @@
 module BottomNavHelper
-  # 下部タブメニューのアイテムを生成する
-  def bottom_nav_item(path:, icon:, label:)
-    active = current_page?(path)
-
-    link_to path,
-      class: "flex flex-col items-center gap-0.5 #{active ? 'text-black font-semibold' : 'text-gray-500'}" do
-      safe_join([
-        content_tag(
-          :span,
-          icon,
-          class: "material-symbols-outlined #{active ? "font-variation-settings-['FILL'_1]" : ''}"
-        ),
-        content_tag(:span, label)
-      ])
-    end
+  # 下部タブ用：選択中なら material-symbols-filled を返す
+  # path: 判定対象のパス
+  def bottom_tab_filled_class(path)
+    "material-symbols-filled" if current_page?(path)
   end
 end
