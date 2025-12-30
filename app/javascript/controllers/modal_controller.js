673bfb6fcdbcbf544410c9c532f98e1315ceb55b
yyoshidaweb <yoshidayumajob@gmail.com>
2025-12-30 17:55:24 +0900

    feat: ステージ作成モーダルを「×」ボタンでクローズ可能に変更

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  // モーダルをクローズする
+  close() {
+    // モーダル全体を取得
+    const frame = this.element.closest("turbo-frame")
+    // モーダル全体の中身を空にする
+    if (frame) frame.innerHTML = ""
+  }
+}
