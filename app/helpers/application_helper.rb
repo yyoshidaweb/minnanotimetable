9120acc2facfae47701b91c755d2593a150ee6b0
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-21 14:37:18 +0900

    feat: main要素のクラスを返すヘルパーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
   def formatted_time(time)
     time&.strftime("%H:%M")
   end
+
+
+  # main要素のクラスを返す
+  def main_element_class
+    if defined?(@timetable_view) && @timetable_view
+      ""
+    elsif defined?(@show_event_header) && @show_event_header
+      "m-2 pt-24"
+    elsif defined?(@page_title) && @page_title.present?
+      "m-2 pt-16"
+    else
+      "m-2 pt-8"
+    end
+  end
 end
