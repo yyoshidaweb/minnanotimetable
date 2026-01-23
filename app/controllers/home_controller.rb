a7aa79ae37be2337c298089af0bd0de6f4779ee9
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-23 17:34:42 +0900

    feat: トップページに「最近作成したタイムテーブル」を表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      @events = current_user.events
+                            .order(created_at: :desc)
+                            .limit(3) # 表示は最大3件まで
+    end
   end
 end
