27cc5ae4aeec59f81ccdaa8e6d56da40b981d3ac
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-23 16:14:17 +0900

    refactor: トップページ表示用コントローラーなどに使用していたStaticPagesという名称をHomeに変更（トップページで動的にタイムテーブル一覧を取得するため）

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :users, only: [ :show ], param: :username
 
   # トップページ
-  root to: "static_pages#index"
+  root to: "home#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
