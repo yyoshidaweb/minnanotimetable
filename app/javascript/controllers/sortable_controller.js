ed0da15768118cca5ffa4ed71366c5ae13aeff62
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-24 21:21:21 +0900

    feat: ステージ並び替え機能を実装

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+
+// ステージ一覧をドラッグ&ドロップで並び替える
+export default class extends Controller {
+    connect() {
+        this.sortable = Sortable.create(this.element, {
+            animation: 150 // 並び替え時のアニメーション
+        })
+    }
+}
