fed27d9f3ce3f45609d8a1cb6bd09c7d7625a618
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-25 18:48:25 +0900

    feat: ステージ並び替え中のアニメーションを調整

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -5,7 +5,7 @@ import Sortable from "sortablejs"
 export default class extends Controller {
     connect() {
         this.sortable = Sortable.create(this.element, {
-            animation: 150 // 並び替え時のアニメーション
+            animation: 150, // 並び替え時のアニメーション
         })
     }
 }
