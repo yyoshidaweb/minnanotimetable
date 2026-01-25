1d9545ead22af5fd8448b7445d6e1c2fb906e5f0
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-26 00:45:01 +0900

    feat: ステージ並び替えページでドラッグ中に画面端に到達した場合、自動スクロールするように変更

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -6,7 +6,12 @@ export default class extends Controller {
     connect() {
         this.sortable = Sortable.create(this.element, {
             animation: 150, // 並び替え時のアニメーション
-            handle: ".stage-handle"
+            handle: ".stage-handle", // ドラッグ可能な部分を指定
+            scroll: true,              // 自動スクロール有効
+            scrollSensitivity: 120,     // 端から何pxで反応するか
+            scrollSpeed: 10,           // スクロール速度
+            scrollContainer: this.element, // スクロールさせるコンテナ
+            forceFallback: true,        // フォールバックモードを強制的に有効化
         })
     }
 }
