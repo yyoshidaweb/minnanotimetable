b3d260f86510837f9a3a9708e19174a40db1fd78
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-27 16:31:53 +0900

    feat: ステージ並び替え機能の自動スクロールスピードを遅くなるように変更

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -8,8 +8,8 @@ export default class extends Controller {
             animation: 150, // 並び替え時のアニメーション
             handle: ".stage-handle", // ドラッグ可能な部分を指定
             scroll: true,              // 自動スクロール有効
-            scrollSensitivity: 120,     // 端から何pxで反応するか
-            scrollSpeed: 10,           // スクロール速度
+            scrollSensitivity: 100,     // 端から何pxで反応するか
+            scrollSpeed: 3,           // スクロール速度
             scrollContainer: this.element, // スクロールさせるコンテナ
             forceFallback: true,        // フォールバックモードを強制的に有効化
         })
