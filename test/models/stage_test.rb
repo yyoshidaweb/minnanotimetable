1e97ff61bc8ea798268781b5cb835de6c3379896
yyoshidaweb <yoshidayumajob@gmail.com>
2026-01-29 19:01:36 +0900

    feat: ステージ作成時にpositionが自動設定されることを確認するテストを作成

--- test/models/stage_test.rb
+++ test/models/stage_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class StageTest < ActiveSupport::TestCase
   def setup
+    @event = events(:one)
     @performer = performers(:one)
     @stage = stages(:one)
     @day = days(:one)
@@ -18,5 +19,16 @@ class StageTest < ActiveSupport::TestCase
     performance.reload
 
     assert_nil performance.stage_id
+  end
+
+  # ステージ作成時にpositionが自動設定される
+  test "assigns position on creation" do
+    max_position = @event.stages.order(:position).last.position
+    new_stage = Stage.new(
+      event: @event,
+      stage_name_tag: stage_name_tags(:three)
+    )
+    new_stage.save!
+    assert_equal max_position + 1, new_stage.position
   end
 end
