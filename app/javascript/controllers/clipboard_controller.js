b16c378c2ae523b1de48b9212b6b68a0b78471f1
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-05 16:03:55 +0900

    fix: URLコピーボタンでコピーされない不具合を修正

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static values = { text: String }
     static targets = ["icon"]
     copy() {
         navigator.clipboard.writeText(this.textValue).then(() => {
