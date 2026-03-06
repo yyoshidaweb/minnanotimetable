35850630c5e026fbb8449dd3f4badac1c8f91a36
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-06 17:30:48 +0900

    chore: コメントを修正

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# クタスターモードで起動しないように設定する
+# クラスターモードで起動しないように設定する
 workers 0
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
