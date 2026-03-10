e12c25f8159a17109db1821b3bb2f5d46374d1b2
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-10 19:21:46 +0900

    feat: 出演情報お気に入り登録と解除機能アクションと単体テストを作成

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,18 +9,8 @@ module ActiveSupport
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # fixturesファイルを上から順番に読み込む
-    fixtures :users
-    fixtures :event_name_tags
-    fixtures :performer_name_tags
-    fixtures :stage_name_tags
-    fixtures :events
-    fixtures :performers
-    fixtures :stages
-    fixtures :days
-    fixtures :performances
-    fixtures :event_favorites
-    fixtures :performer_favorites
+    # fixturesを全て読み込む
+    fixtures :all
 
     # Add more helper methods to be used by all tests here...
   end
