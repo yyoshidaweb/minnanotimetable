a0974f53d8eea0ae61f0a0340d7f202c5008d9ca
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-11 12:00:44 +0900

    feat: N+1を回避するため、performers取得時にperformancesをincludesで取得

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -9,7 +9,7 @@ class PerformersController < ApplicationController
   before_action :show_event_header, except: %i[ destroy ]
 
   def index
-    @performers = @event.performers
+    @performers = @event.performers.includes(:performances)
     # お気に入り登録している出演者IDの配列を取得
     if user_signed_in?
       @favorite_performer_map = current_user.favorite_performer_map
