c6f17730ae5d38d1cf86929acf5bc24519b749ed
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-16 01:58:14 +0900

    feat: マイタイテの場合にevent-headerのタイトルを変更するためのヘルパーを作成

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,4 +3,13 @@ module EventsHelper
   def event_header_color
     @my_timetable_view ? "bg-orange-600" : "bg-gray-800"
   end
+
+  # event-headerのタイトルを返す
+  def event_header_title
+    if @my_timetable_view
+      "#{@user.name}の#{@event.display_name} マイタイムテーブル"
+    else
+      "#{@event.display_name} タイムテーブル"
+    end
+  end
 end
