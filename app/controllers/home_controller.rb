f9cc89b9b4befe9096b6af7617ddcf0641058350
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-24 18:01:52 +0900

    feat: トップページの見た目を調整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @events = Event.popular_for_home
-    @created_events = Event.recent_created_for_home(current_user)
-    @favorite_events = Event.recent_favorite_for_home(current_user)
+    if user_signed_in?
+      @created_events = Event.recent_created_for_home(current_user)
+      @favorite_events = Event.recent_favorite_for_home(current_user)
+    end
   end
 end
