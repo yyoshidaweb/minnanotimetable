7cdb40435c14fa962eb3a918a84e055f4fd4d269
yyoshidaweb <yoshidayumajob@gmail.com>
2026-03-30 19:09:33 +0900

    feat: text内にリンクが含まれていたら自動リンク化するヘルパーを作成し、タイムテーブル概要タブの説明に適用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,22 @@ module ApplicationHelper
       "m-2 pt-8"
     end
   end
+
+  # text内にリンクが含まれていたら自動リンク化する
+  def auto_link_text(text)
+    return "" if text.blank?
+    # URLをリンク化し、XSSを防ぐ
+    linked = Rinku.auto_link(
+      text,
+      :urls,
+      'target="_blank" rel="noopener"
+      class="text-blue-600 underline hover:text-blue-800 break-all"'
+    )
+    sanitized = sanitize(
+      linked,
+      tags: %w[a br],
+      attributes: %w[href target rel class]
+    )
+    content_tag(:p, sanitized.html_safe.html_safe, class: "text-gray-800 whitespace-pre-line")
+  end
 end
