34553b80bb9241b6d858c57ebe4aae01a273fb1c
yyoshidaweb <yoshidayumajob@gmail.com>
2026-04-11 10:27:59 +0900

    chore: タイムテーブル作成サービスクラスのテストから余分なsetupを削除

--- test/services/timetable_creator_test.rb
+++ test/services/timetable_creator_test.rb
@@ -1,19 +1,8 @@
 require "test_helper"
 
 class TimetableCreatorTest < ActionDispatch::IntegrationTest
-  # Devise のテストヘルパーをインクルード
-  include Devise::Test::IntegrationHelpers
-
   setup do
-    # Google 認証のテスト用ユーザーを作成
-    @user = users(:one)
-    @user_two = users(:two)
-    # テスト用のログイン状態を再現
-    sign_in @user
-    @event = events(:one)
     @no_performance_event = events(:no_performance_event)
-    @day1 = days(:one)
-    @day2 = days(:two)
     @no_performance_event_day = days(:no_performance_event_day)
     @json = JSON.parse(file_fixture("timetable_json.json").read)
     @timetable_json_without_stages = JSON.parse(file_fixture("timetable_json_without_stages.json").read)
