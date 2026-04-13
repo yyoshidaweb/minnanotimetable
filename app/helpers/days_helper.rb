f4c61b46077c082359fe2a5138fa97b5facef26a
yyoshidaweb <yoshidayumajob@gmail.com>
2026-04-13 20:25:38 +0900

    feat: day作成モーダルの実装を共通化するため、変数form_typeでモーダル表示元を判断し、表示を分岐する設計に変更

--- app/helpers/days_helper.rb
+++ app/helpers/days_helper.rb
@@ -1,2 +1,11 @@
 module DaysHelper
+  # フォームタイプごとのselect_tagを返す
+  def day_select_name(form_type)
+    case form_type
+    when "performance"
+      "performance[day_id]"
+    else
+      "day_id"
+    end
+  end
 end
