66b6f464c98b849a897bcff91875961da7d4ad9e
yyoshidaweb <yoshidayumajob@gmail.com>
2026-04-30 20:13:08 +0900

    feat: トップページの「みんなが作ったタイムテーブル」に過去イベントも表示するように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.future_all.limit(5)
+      future = Event.future_all.limit(10).to_a
+      remaining_size = 10 - future.size
+      past = remaining_size > 0 ? Event.past_all.limit(remaining_size).to_a : []
+      @events = (future + past)
     if user_signed_in?
       @created_events = Event.recent_created_for_home(current_user)
       @favorite_events = Event.recent_favorite_for_home(current_user)
